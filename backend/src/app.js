370843df8757fddb8f2a8a30e2802e4569400ffa
root <root@dlunagariya@surekhatech.com>
2025-03-27 15:28:16 +0530

    a

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,8 +6,12 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
+    // origin: process.env.CORS_ORIGIN,
+    // credentials: true
+    origin: "*", // Allow all origins (Not recommended for production)
+    credentials: true,
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: ["Content-Type", "Authorization"]
 }))
 
 app.use(cookieParser())
